564469ca6edae4a62324484841c18017c6e1f045
Caterina Gazzotti <gazzotti.caterina@gmail.com>
2024-08-28 11:29:33 +0200

    🔨 Update script due to dockerfiles changes

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-conda activate tf
-docker-compose up &
+#pip install -r requirements.txt
+sudo docker-compose up &
 yarn run dev &
+
 cd connection
-python3 minio_api.py &
-python3 neo4j_api.py &
-cd ../statistics
-docker build -t statistics -f Dockerfile .
-docker run -p 5003:5003 --network=sindit_network -it statistics &
+python minio_api.py & 
+python neo4j_api.py & 
+python analytics_api.py & 
+python influxdb_api.py &
