e2a6c8620848c95f49388c95ae8ae96a67e4508c
Caterina Gazzotti <gazzotti.caterina@gmail.com>
2024-08-30 13:27:15 +0200

    🔥 remove previous project

--- src/settings/SettingsActions.ts
+++
@@ -1,5 +0,0 @@
-export const UPDATE_DASHBOARD_SETTING = 'SETTINGS/UPDATE_DASHBOARD_SETTING';
-export const updateDashboardSetting = (setting: string, value: any) => ({
-  type: UPDATE_DASHBOARD_SETTING,
-  payload: { setting, value },
-});
